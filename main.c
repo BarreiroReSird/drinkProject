eb4c594de38b0bf13ace7f2ba296895de255c41a
Carlos Barreiro <carlosbarreiro2604@gmail.com>
2021-12-07 00:56:08 +0000

    feat: 🎸 100% functional project
    The project now works 100%, but it doesn't have anything that locks the
    code to what is requested, in other words, if we put in something
    random, it is very easy to crash the project.

--- main.c
+++ main.c
@@ -30,7 +30,7 @@ int createDrink(vDrink *drinksList, int *drinksNumber, int *lastDrinkId) {
     fflush(stdin);
     for (int i = 0; i < drinksToRead; i++) {
         printf("\n Nome da bebida: \t");
-        scanf("%s", &drink.dName);
+        scanf("%s", drink.dName);
         fflush(stdin);
 
         printf("\n Preco da bebida: \t");
@@ -178,21 +178,55 @@ void averagePriceOfDrinks(vDrink *vDrinks, int drinksNumber) {
 
 //Mostrar bebida por tipo inserido
 void drinksByType(vDrink *vDrinks, int drinksNumber) {
+    char requestedType[MAXSTR];
 
-    //Vars
-    char requestedType[TAM] = "none";
-
-    //MainCode
-    printf("\n Insira o tipo de bebida: (simples|alcool|refrigerante|outro)\t");
+    printf("\n Insira o tipo de bebida: (simples|alcool|refrigerante)\t");
     scanf("%s", requestedType);
     for (int i = 0; i < drinksNumber; i++) {
-        if (vDrinks[i].dType == requestedType) {
-            printf("\n APAGAR - requestedType encontrado \t");
+        if (strcmp(vDrinks[i].dType, requestedType) == 0) {
             printf("\n Id: %d", vDrinks[i].dId);
             printf(" | Nome: %s", vDrinks[i].dName);
             printf(" | Preco: %.2f", vDrinks[i].dPrice);
             printf(" | Stock: %d", vDrinks[i].dStock);
             printf(" | Tipo: %s", vDrinks[i].dType);
+        } else {
+            printf("\n Todas as bebidas de todos os tipos, pois nao escolheu nenhuma das opcoes possiveis:");
+            printf("\n Id: %d", vDrinks[i].dId);
+            printf(" | Nome: %s", vDrinks[i].dName);
+            printf(" | Preco: %.2f", vDrinks[i].dPrice);
+            printf(" | Stock: %d", vDrinks[i].dStock);
+            printf(" | Tipo: %s", vDrinks[i].dType);
+        }
+    }
+}
+
+//Mostrar a bebida mais barata de um certo tipo
+void cheapestDrinkOfCertainType(vDrink *vDrinks, int drinksNumber) {
+    char requestedType[MAXSTR];
+    float cheapestDrink = 999;
+    int iVar = 0;
+
+    printf("\n Insira o tipo de bebida: (simples|alcool|refrigerante)\t");
+    scanf("%s", requestedType);
+    for (int i = 0; i < drinksNumber; i++) {
+        if (strcmp(vDrinks[i].dType, requestedType) == 0) {
+            if (cheapestDrink > vDrinks[i].dPrice) {
+                cheapestDrink = vDrinks[i].dPrice;
+                iVar = i;
+            }
+        } else {
+            printf("\n Nenhum dos tipos possiveis foi escolhido \t");
+        }
+    }
+    for (int i = 0; i < drinksNumber; i++) {
+        if (strcmp(vDrinks[i].dType, requestedType) == 0) {
+            printf("\n Bebida mais barata desse tipo: \t");
+            printf("\n Id: %d", vDrinks[iVar].dId);
+            printf(" | Nome: %s", vDrinks[iVar].dName);
+            printf(" | Preco: %.2f", vDrinks[iVar].dPrice);
+            printf(" | Stock: %d", vDrinks[iVar].dStock);
+            printf(" | Tipo: %s", vDrinks[iVar].dType);
+            break;
         }
     }
 }
@@ -220,23 +254,86 @@ void overpricedDrinks(vDrink *vDrinks, int drinksNumber) {
     printf("\n Media de precos: %.2f\t", averagePrice);
 }
 
+//Contabilizar, para cada tipo de bebida, a media da quantidade de garrafas em stock
+void averageNumberOfBottlesInStockPerType(vDrink *vDrinks, int drinksNumber) {
+    char simple[MAXSTR] = "simples", alcohol[MAXSTR] = "alcool", soda[MAXSTR] = "refrigerante";
+    int stockCount = 0, stockTypeS = 0, stockTypeA = 0, stockTypeR = 0;
+    float averageTypeS = 0, averageTypeA = 0, averageTypeR = 0;
+
+    for (int i = 0; i < drinksNumber; i++) {
+        stockCount = stockCount + vDrinks[i].dStock;
+    }
+
+    for (int i = 0; i < drinksNumber; i++) {
+        if (strcmp(vDrinks[i].dType, simple) == 0) {
+            stockTypeS = stockTypeS + vDrinks[i].dStock;
+        }
+    }
+
+    for (int i = 0; i < drinksNumber; i++) {
+        if (strcmp(vDrinks[i].dType, alcohol) == 0) {
+            stockTypeA = stockTypeA + vDrinks[i].dStock;
+        }
+    }
+
+    for (int i = 0; i < drinksNumber; i++) {
+        if (strcmp(vDrinks[i].dType, soda) == 0) {
+            stockTypeR = stockTypeR + vDrinks[i].dStock;
+        }
+    }
+
+    averageTypeS = stockTypeS * 100;
+    averageTypeS = averageTypeS / stockCount;
+    averageTypeA = stockTypeA * 100;
+    averageTypeA = averageTypeA / stockCount;
+    averageTypeR = stockTypeR * 100;
+    averageTypeR = averageTypeR / stockCount;
+
+    printf("\n Media de bebidas: Simples - %.2f%%\t", averageTypeS);
+    printf("\n Media de bebidas: Alcool - %.2f%%\t", averageTypeA);
+    printf("\n Media de bebidas: Refrigerante - %.2f%%\t", averageTypeR);
+}
+
+//Vender uma bebida por id
+void sellDrinksById(vDrink *vDrinks, int drinksNumber, int *lastDrinkId) {
+    int x = *lastDrinkId, requestedId, stock = 0, quantityToBuy = 0;
+
+    printf("\n Ultimo id inserido: %d\t", x);
+    printf("\n Insira o id da bebida: \t");
+    scanf("%d", &requestedId);
+    for (int i = 0; i < drinksNumber; i++) {
+        if (vDrinks[i].dId==requestedId) {
+            stock = vDrinks[i].dStock;
+            printf("\n Quantas deseja comprar?: \t");
+            scanf("%d", &quantityToBuy);
+            if (quantityToBuy <= stock) {
+                vDrinks[i].dStock = stock - quantityToBuy;
+                printf("\n Id: %d", vDrinks[i].dId);
+                printf(" | Nome: %s", vDrinks[i].dName);
+                printf(" | Preco: %.2f", vDrinks[i].dPrice);
+                printf(" | Stock: %d", vDrinks[i].dStock);
+                printf(" | Tipo: %s", vDrinks[i].dType);
+            } else {
+                printf("\n O stock nao e suficiente \t");
+            } 
+        }
+    }
+}
+
 //Mostrar valor do iva de todo o stock de alccol
 void calculateIvaAlcool(vDrink *vDrinks, int drinksNumber) {
-
-    //Vars
+    char alcohol[MAXSTR] = "alcool";
     float priceAndIva = 0, ivaAlcool = 0, justPrice = 0;
 
-    //MainCode
     for (int i = 0; i < drinksNumber; i++) {
-        if (vDrinks[i].dType == "alcool") {
-            printf("\n vDrinks[i].dType: %.2f \t", vDrinks[i].dType);     //apagar
+        if (strcmp(vDrinks[i].dType, alcohol) == 0) {
             priceAndIva = priceAndIva + vDrinks[i].dPrice;
         }
     }
     justPrice = priceAndIva / 1.13;
     ivaAlcool = priceAndIva - justPrice;
-    printf("\n priceAndIva: %.2f \t", priceAndIva); //apagar
-    printf("\n justPrice: %.2f \t", justPrice);     //apagar
+    printf("\n priceAndIva: %.2f \t", priceAndIva);
+    printf("\n justPrice: %.2f \t", justPrice);
     printf("\n O preco do iva do alcool em stock: %.2f \t", ivaAlcool);
 }
 
@@ -249,24 +346,22 @@ int main() {
         lastDrinkAddedId = 0;
     vDrink drinks[TAM];
 
-	//Preencher com variaveis restantes
-
 	//main code
 	puts("\n Gestao de bebidas:");              
-	puts("\n 01|Adicionar bebidas");                                                                    //Funciona: sim
-    puts("\n 02|Listar inventario de bebidas");                                                         //Funciona: sim
-    puts("\n 03|Listar bebida individual");                                                             //Funciona: sim
-    puts("\n 04|Alterar preco de uma bebida por id");                                                   //Funciona: sim
-    puts("\n 05|Mostrar bebida com mais stock");                                                        //Funciona: sim
-    puts("\n 06|Mostrar todo o stock (quantidade e preco)");                                            //Funciona: sim
-    puts("\n 07|Media de preco das bebidas");                                                           //Funciona: sim
-    puts("\n 08|Listar todas as bebidas por tipo (simples|alcool|refrigerante|outros)");                //Funciona: falha
-    puts("\n 09|Mostrar a bebida mais barata de um certo tipo");                                        //Funciona: nao
-    puts("\n 10|Listar bebidas que o preco esta acima da media");                                       //Funciona: sim
-    puts("\n 11|Contabilizar, para cada tipo de bebida, a media da quantidade de garrafas em stock");   //Funciona: nao
-    puts("\n 12|Vender bebidas por id");                                                                //Funciona: nao
-    puts("\n 13|Calcular o IVA de todo o stock de alcool");                                             //Funciona: falha
-    puts("\n 00|Sair");                                                                                 //Funciona: sim
+	puts("\n 01|Adicionar bebidas");                                                                    
+    puts("\n 02|Listar inventario de bebidas");                                                         
+    puts("\n 03|Listar bebida individual");                                                            
+    puts("\n 04|Alterar preco de uma bebida por id");                                                   
+    puts("\n 05|Mostrar bebida com mais stock");                                                        
+    puts("\n 06|Mostrar todo o stock (quantidade e preco)");                                            
+    puts("\n 07|Media de preco das bebidas");                                                           
+    puts("\n 08|Listar todas as bebidas por tipo (simples|alcool|refrigerante|outros)");               
+    puts("\n 09|Mostrar a bebida mais barata de um certo tipo");                                        
+    puts("\n 10|Listar bebidas que o preco esta acima da media");                                       
+    puts("\n 11|Contabilizar, para cada tipo de bebida, a media da quantidade de garrafas em stock");   
+    puts("\n 12|Vender bebidas por id");                                                                
+    puts("\n 13|Calcular o IVA de todo o stock de alcool");                                            
+    puts("\n 00|Sair");                                                                                
 
     printf("\n Escolha a opcao que deseja: \t");
     scanf("%d", &menu);
@@ -309,19 +404,19 @@ int main() {
             break;
 
         case 9: //Mostrar a bebida mais barata de um certo tipo
-            //Coming Soon
+            cheapestDrinkOfCertainType(drinks, drinksNumber);
             break;
 
         case 10: //Mostrar bebidas com preco acima da media
             overpricedDrinks(drinks, drinksNumber);
             break;
 
-        case 11: //
-            //Coming Soon
+        case 11: //Contabilizar, para cada tipo de bebida, a media da quantidade de garrafas em stock
+            averageNumberOfBottlesInStockPerType(drinks, drinksNumber);
             break;
 
-        case 12: //
-            //Coming Soon
+        case 12: //Vender bebidas por id
+            sellDrinksById(drinks, drinksNumber, &lastDrinkAddedId);
             break;
 
         case 13: //Calcular iva do alcool em stock
@@ -339,20 +434,20 @@ int main() {
 
 	    //main code
 	    puts("\n Gestao de bebidas:");              
-	    puts("\n 01|Adicionar bebidas");                                                                 
-        puts("\n 02|Listar inventario de bebidas");                                                        
-        puts("\n 03|Listar bebida individual");                                                             
+	    puts("\n 01|Adicionar bebidas");                                                                    
+        puts("\n 02|Listar inventario de bebidas");                                                         
+        puts("\n 03|Listar bebida individual");                                                            
         puts("\n 04|Alterar preco de uma bebida por id");                                                   
-        puts("\n 05|Mostrar bebida com mais stock");                                                      
-        puts("\n 06|Mostrar todo o stock (quantidade e preco)");                                         
-        puts("\n 07|Media de preco das bebidas");                                                        
-        puts("\n 08|Listar todas as bebidas por tipo (simples|alcool|refrigerante|outros)");            
-        puts("\n 09|Mostrar a bebida mais barata de um certo tipo");                                       
-        puts("\n 10|Listar bebidas que o preco esta acima da media");                                      
+        puts("\n 05|Mostrar bebida com mais stock");                                                        
+        puts("\n 06|Mostrar todo o stock (quantidade e preco)");                                            
+        puts("\n 07|Media de preco das bebidas");                                                           
+        puts("\n 08|Listar todas as bebidas por tipo (simples|alcool|refrigerante|outros)");               
+        puts("\n 09|Mostrar a bebida mais barata de um certo tipo");                                        
+        puts("\n 10|Listar bebidas que o preco esta acima da media");                                       
         puts("\n 11|Contabilizar, para cada tipo de bebida, a media da quantidade de garrafas em stock");   
-        puts("\n 12|Vender bebidas por id");
-        puts("\n 13|Calcular o IVA de todo oo stock de alcool");
-        puts("\n 00|Sair");
+        puts("\n 12|Vender bebidas por id");                                                                
+        puts("\n 13|Calcular o IVA de todo o stock de alcool");                                            
+        puts("\n 00|Sair");  
 
         printf("\n Escolha a opcao que deseja: \t");
         scanf("%d", &menu);
